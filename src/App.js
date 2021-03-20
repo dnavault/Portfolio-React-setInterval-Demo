79088c22bd454ccb7195dccfd2ec5dd15fa4f491
dnavault <47720676+dnavault@users.noreply.github.com>
2021-03-20 10:28:34 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import "./styles.css";
 
 export default function App() {
   const [imgSrc, changeImgSrc] = useState("");
+  const [imgTv, changeImgTv] = useState("");
 
   useEffect(() => {
     const interval = setInterval(() => {
       fetch("	https://dog.ceo/api/breeds/image/random")
         .then((response) => response.json())
         .then((data) => changeImgSrc(data.message));
+
+      fetch("https://v2.jokeapi.dev/joke/Any")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
     }, 9000);
 
     return () => clearInterval(interval);

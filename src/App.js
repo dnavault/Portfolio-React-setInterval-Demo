46894ece78b5dc56094519f25f095b99a723d548
dnavault <47720676+dnavault@users.noreply.github.com>
2021-03-20 13:01:35 -0400

    MoreChnag

--- src/App.js
+++ src/App.js
@@ -7,21 +7,30 @@ export default function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch("	https://dog.ceo/api/breeds/image/random")
+      fetch("https://dog.ceo/api/breeds/image/random")
         .then((response) => response.json())
-        .then((data) => changeImgSrc(data.message));
+        .then((data) => {
+          changeImgSrc(data.message);
+          console.log(data);
+        });
 
       fetch("https://v2.jokeapi.dev/joke/Any")
         .then((response) => response.json())
         .then((data) => console.log(data));
-    }, 9000);
+    }, 9999);
 
     return () => clearInterval(interval);
   }, []);
 
+  getUrls = () => {
+    fetch("FeedUrls.txt").then((response) => response);
+  };
+
   return (
     <div className="App">
-      <img src={imgSrc} />
+      <div class="dog-random-img-container">
+        <img src={imgSrc} alt="" />
+      </div>
     </div>
   );
 }
